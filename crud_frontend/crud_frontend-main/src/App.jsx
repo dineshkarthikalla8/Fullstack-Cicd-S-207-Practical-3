774583365bef1ffdd0d4f7694163ac8d3de46e94
Alla Dinesh Karthik <dineshkarthikalla@gmail.com>
2025-08-11 23:51:20 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -49,7 +49,7 @@ const BASE_URL = 'http://100.26.99.38:9090/springapp1';
  
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
-      await axios.delete(`http://localhost:9090/delete/${id}`);
+      await axios.delete(`http://100.26.99.38:9090/springapp1/delete/${id}`);
       alert('Delete successful');
       fetchProducts();
     }
