2ef9f87302829cc83bd1a42db83decad8024701c
Alla Dinesh Karthik <dineshkarthikalla@gmail.com>
2025-08-11 23:39:33 +0530

    Update Service.java

--- crud_backend/crud_backend-main/src/main/java/com/klu/Service.java
+++ crud_backend/crud_backend-main/src/main/java/com/klu/Service.java
@@ -1,40 +1,40 @@
 package com.klu;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
+@org.springframework.stereotype.Service
 public class Service {
+	@Autowired
+	Repo r1;
 
-    @Autowired
-    Repo r1;
+	public String insertData(Product p) {
+		r1.save(p);
+		return "Insert success";
+	}
 
-    public String insertData(Product p) {
-        r1.save(p);
-        return "Insert success";
-    }
+	public List<Product> displayData() {
+		return r1.findAll();
+	}
 
-    public List<Product> displayData() {
-        return r1.findAll();
-    }
+	public String updateData(Product p) {
+		if (r1.existsById(p.getId())) {
+			r1.save(p);
+			return "Update success";
+		} else {
+			return "Product not found";
+		}
+	}
 
-    public String updateData(Product p) {
-        if (r1.existsById(p.getId())) {
-            r1.save(p);
-            return "Update success";
-        } else {
-            return "Product not found";
-        }
-    }
-
-    // ✅ Added missing deleteData method
-    public String deleteData(int id) {
-        if (r1.existsById(id)) {
-            r1.deleteById(id);
-            return "Delete success";
-        } else {
-            return "Product not found";
-        }
-    }
+	/*
+	public String deleteData(int id) {
+		if (r1.existsById(id)) {
+			r1.deleteById(id);
+			return "Delete success";
+		} else {
+			return "Product not found";
+		}
+	}
+	*/
 }
