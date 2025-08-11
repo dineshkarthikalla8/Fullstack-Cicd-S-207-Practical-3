03165dbfac1968b6a944c384819918f2ae3b0813
Alla Dinesh Karthik <dineshkarthikalla@gmail.com>
2025-08-11 23:35:06 +0530

    Update Service.java

--- crud_backend/crud_backend-main/src/main/java/com/klu/Service.java
+++ crud_backend/crud_backend-main/src/main/java/com/klu/Service.java
@@ -1,31 +1,40 @@
 package com.klu;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@org.springframework.stereotype.Service
+@Service
 public class Service {
-	@Autowired
-	Repo r1;
 
-	public String insertData(Product p) {
-		r1.save(p);
-		return "Insert success";
-	}
+    @Autowired
+    Repo r1;
 
-	public List<Product> displayData() {
-		return r1.findAll();
-	}
+    public String insertData(Product p) {
+        r1.save(p);
+        return "Insert success";
+    }
 
-	public String updateData(Product p) {
-		if (r1.existsById(p.getId())) {
-			r1.save(p);
-			return "Update success";
-		} else {
-			return "Product not found";
-		}
-	}
+    public List<Product> displayData() {
+        return r1.findAll();
+    }
 
-	
+    public String updateData(Product p) {
+        if (r1.existsById(p.getId())) {
+            r1.save(p);
+            return "Update success";
+        } else {
+            return "Product not found";
+        }
+    }
+
+    // ✅ Added missing deleteData method
+    public String deleteData(int id) {
+        if (r1.existsById(id)) {
+            r1.deleteById(id);
+            return "Delete success";
+        } else {
+            return "Product not found";
+        }
+    }
 }
